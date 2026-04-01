5a7b1f5429d02fbc39372989b06d6e4e07b59b1a
HABIB SHEHU <habibshehuoooo7@gmail.com>
2026-04-01 15:43:45 +0100

    Re updating the structure of the Application

--- velora-backend/routes/profile.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getProfile } = require("../controllers/profileController");
-const { authenticateUser } = require("../middleware/authMiddleware");
-
-router.get("/", authenticateUser, getProfile);
-
-module.exports = router;
